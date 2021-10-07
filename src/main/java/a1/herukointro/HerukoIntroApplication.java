75c128e5e59298269f6c1ecdf30f7152176be2a0
Christoffer <christofferp97@hotmail.com>
2021-10-07 15:55:18 +0200

    ny text

--- src/main/java/a1/herukointro/HerukoIntroApplication.java
+++ src/main/java/a1/herukointro/HerukoIntroApplication.java
@@ -2,6 +2,8 @@ package a1.herukointro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class HerukoIntroApplication {
@@ -10,4 +12,13 @@ public class HerukoIntroApplication {
     SpringApplication.run(HerukoIntroApplication.class, args);
   }
 
+
+}
+@RestController
+class helloController{
+
+  @GetMapping("/")
+  String hello()  {
+    return "This is it";
+  }
 }
