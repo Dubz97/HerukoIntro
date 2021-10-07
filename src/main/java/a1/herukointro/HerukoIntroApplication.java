c9aebb6b7dab252b1a902d67ddee13a13ca547a0
Christoffer <christofferp97@hotmail.com>
2021-10-07 15:44:15 +0200

    tester

---
+++ src/main/java/a1/herukointro/HerukoIntroApplication.java
@@ -0,0 +1,13 @@
+package a1.herukointro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerukoIntroApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HerukoIntroApplication.class, args);
+  }
+
+}
---
+++ src/test/java/a1/herukointro/HerukoIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package a1.herukointro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerukoIntroApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
